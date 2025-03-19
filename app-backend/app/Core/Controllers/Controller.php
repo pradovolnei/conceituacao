018917f2952cc039fb235145d582daad18c20e8f
Uhelliton Andrade <uhelliton@uol.com.br>
2025-03-18 23:35:52 -0300

    refactor(app/structure): DDD (Domain-Driven Design) Lite Architecture

--- app-backend/app/Core/Controllers/Controller.php
+++ app-backend/app/Core/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Controllers;
 
 abstract class Controller
 {
--- app-backend/bootstrap/providers.php
+++ app-backend/bootstrap/providers.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
+    \App\Core\Providers\AppServiceProvider::class,
 ];
