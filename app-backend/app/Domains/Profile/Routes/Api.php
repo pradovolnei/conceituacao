0e3a43097c819b5d9c7332839c16158a4275b429
Uhelliton Andrade <uhelliton@uol.com.br>
2025-03-20 10:40:28 -0300

    feat(middleware/AuthorizeAdministrator): create middleware + guard.administrator to routes: profiles

--- app-backend/app/Domains/Profile/Routes/Api.php
+++ app-backend/app/Domains/Profile/Routes/Api.php
@@ -5,9 +5,11 @@ use App\Domains\Profile\Controllers\ProfileUserController;
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('/profiles', ProfileController::class);
+    Route::middleware(['guard.administrator'])->group(function () {
+        Route::apiResource('/profiles', ProfileController::class);
 
-    Route::post('/profiles/attach', [ProfileUserController::class, 'attachProfile']);
-    Route::post('/profiles/detach', [ProfileUserController::class, 'detachProfile']);
-    Route::get('/profiles/users/{id}', [ProfileUserController::class, 'getProfilesByUser']);
+        Route::post('/profiles/attach', [ProfileUserController::class, 'attachProfile']);
+        Route::post('/profiles/detach', [ProfileUserController::class, 'detachProfile']);
+        Route::get('/profiles/users/{id}', [ProfileUserController::class, 'getProfilesByUser']);
+    });
 });
---
+++ app-backend/app/Infra/Enums/ProfileType.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Infra\Enums;
+
+enum ProfileType: string
+{
+    case Admin = 'Administrador';
+}
