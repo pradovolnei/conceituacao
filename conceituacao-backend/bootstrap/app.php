a8b58f650188f9438d649723154b81fdfe750be1
Volnei Prado <valnei.prado@FOREVERLIVING.LOCAL>
2025-08-26 15:47:06 -0300

    feat(access-control): middleware CheckAdminProfile e aplicacao nas rotas

--- conceituacao-backend/bootstrap/app.php
+++ conceituacao-backend/bootstrap/app.php
@@ -18,9 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'admin' => \App\Http\Middleware\CheckAdminProfile::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
